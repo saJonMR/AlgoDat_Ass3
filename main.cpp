ca8b5597374dc29db44b4deb1d26cbbb9e8e0815
Jonas Roquette <roquettejonas@gmail.com>
2020-06-25 17:11:55 +0200

    Weird too

--- main.cpp
+++ main.cpp
@@ -7,8 +7,40 @@
 int main() {
     NodeTemplate Berlin("Berlin");
     NodeTemplate Hamburg("Hamburg");
+    NodeTemplate Frankfurt("Frankfurt");
+    NodeTemplate Weimar("Weimar");
+    NodeTemplate Muenchen("Muenchen");
 
-    Berlin.add_connection(Hamburg, 400);
+    Berlin.add_connection(Hamburg, -200);
+    Berlin.add_connection(Frankfurt, 550);
+    Berlin.add_connection(Weimar, 300);
+    Berlin.add_connection(Muenchen, 2000);
+    Hamburg.add_connection(Frankfurt, 250);
+    Frankfurt.add_connection(Muenchen, 650);
 
-    std::cout << Berlin.connected();
+    GraphTemplate Map(Berlin);
+
+    Map.add(Hamburg);
+    Map.add(Frankfurt);
+    Map.add(Weimar);
+    Map.add(Muenchen);
+       
+    std::cout << Berlin.dist() << std::endl;
+    std::cout << Hamburg.dist() << std::endl;
+    std::cout << Frankfurt.dist() << std::endl;
+    std::cout << Weimar.dist() << std::endl;
+    std::cout << Muenchen.dist() << std::endl;
+
+    //std::cout << Map.listNode(2);
+    std::cout << Map.find("Hamburg");
+
+    std::cout << Map.bellman_fort(Berlin) << std::endl;
+
+   
+    std::cout << Berlin.dist() << std::endl;
+    std::cout << Hamburg.dist() << std::endl;
+    std::cout << Frankfurt.dist() << std::endl;
+    std::cout << Weimar.dist() << std::endl;
+    std::cout << Muenchen.dist() << std::endl;
+    
 }
